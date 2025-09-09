53b1a40a970f7899e1fd323b5fa97eba565f31e5
Bgs28 <157942880+Bgs28@users.noreply.github.com>
2025-09-09 05:47:32 +0000

    membuat modul 1

---
+++ modul.mjs
@@ -0,0 +1,12 @@
+// export fungsi secara default
+
+// export default function myfungsi() {
+//     console.log("Hello World");
+// }
+
+// export variabel
+const name = "Bagas";
+const address = "Jl. Merdeka No. 1";
+const age = 22;
+
+export { name, address, age };
